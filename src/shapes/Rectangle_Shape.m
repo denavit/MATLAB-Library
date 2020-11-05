1f425bf3e0f62e88592d5e9f7213dac05cbafd2a
denavit <denavit@gmail.com>
2020-11-05 10:32:56 -0500

    Renaming corner radius variable

--- src/shapes/Rectangle_Shape.m
+++ src/shapes/Rectangle_Shape.m
@@ -1,26 +1,26 @@
 classdef Rectangle_Shape < geometric_shape
         
     properties
-        H
-        B
-        r = 0;
+        H       % Height
+        B       % Width
+        rc = 0; % Corner radius
         
         plot_fill_color = [0.90 0.90 0.90];
     end
     
     methods
-        function obj = Rectangle_Shape(H,B,r)
+        function obj = Rectangle_Shape(H,B,rc)
             obj.H = H;
             obj.B = B;
             if nargin > 2
-                obj.r = r;
+                obj.rc = rc;
             end
         end 
         function tf = is_section_valid(obj)
             tf1 = obj.B > 0;    % B should be positive
             tf2 = obj.H > 0;    % H should be positive
-            tf3 = obj.r >= 0;   % r should be positive or zero
-            tf4 = obj.r < min([obj.B obj.H])/2;
+            tf3 = obj.rc >= 0;  % rc should be positive or zero
+            tf4 = obj.rc < min([obj.B obj.H])/2;
             tf = all([tf1 tf2 tf3 tf4]);
         end
         function d = depth(obj,axis)
@@ -34,10 +34,10 @@ classdef Rectangle_Shape < geometric_shape
             end            
         end
         function a = A(obj)
-            a = obj.H*obj.B - (4-pi)*obj.r^2;
+            a = obj.H*obj.B - (4-pi)*obj.rc^2;
         end
         function j = J(obj)
-            assert(obj.r==0,'Not yet implemented for rounded rectangles');
+            assert(obj.rc==0,'Not yet implemented for rounded rectangles');
             % Equation for J from Theory of Elasticity by Timoshenko and Goodier 
             % (first two terms of the infinite series).
             % See also: Plaut, R. H., and Eatherton, M. R. (2017). 
@@ -56,22 +56,22 @@ classdef Rectangle_Shape < geometric_shape
         function i = I(obj,axis)
             switch lower(axis)
                 case {'x','z','major','strong'}
-                    if obj.r == 0
+                    if obj.rc == 0
                         i = (1/12)*obj.B*obj.H^3;
                     else
                         i = (1/12)*obj.B*obj.H^3 ...
-                            - 4 * ((1/12)*obj.r^4 + obj.r^2*(obj.H/2-obj.r/2)^2) ...
-                            + 4 * ((pi/16-4/(9*pi))*obj.r^4 ...
-                            + (pi/4)*obj.r^2*(obj.H/2-(obj.r-(4*obj.r)/(3*pi)))^2);
+                            - 4 * ((1/12)*obj.rc^4 + obj.rc^2*(obj.H/2-obj.rc/2)^2) ...
+                            + 4 * ((pi/16-4/(9*pi))*obj.rc^4 ...
+                            + (pi/4)*obj.rc^2*(obj.H/2-(obj.rc-(4*obj.rc)/(3*pi)))^2);
                     end
                 case {'y','minor','weak'}
-                    if obj.r == 0
+                    if obj.rc == 0
                         i = (1/12)*obj.H*obj.B^3;
                     else
                         i = (1/12)*obj.H*obj.B^3 ...
-                            - 4 * ((1/12)*obj.r^4 + obj.r^2*(obj.B/2-obj.r/2)^2) ...
-                            + 4 * ((pi/16-4/(9*pi))*obj.r^4 ...
-                            + (pi/4)*obj.r^2*(obj.B/2-(obj.r-(4*obj.r)/(3*pi)))^2);
+                            - 4 * ((1/12)*obj.rc^4 + obj.rc^2*(obj.B/2-obj.rc/2)^2) ...
+                            + 4 * ((pi/16-4/(9*pi))*obj.rc^4 ...
+                            + (pi/4)*obj.rc^2*(obj.B/2-(obj.rc-(4*obj.rc)/(3*pi)))^2);
                     end
                 otherwise
                     error('Unknown axis: %s',axis);
@@ -91,25 +91,25 @@ classdef Rectangle_Shape < geometric_shape
         function z = Z(obj,axis)
             switch lower(axis)
                 case {'x','z','major','strong'}
-                    if obj.r == 0
+                    if obj.rc == 0
                         z = obj.B*obj.H^2/4;
                     else
                         z = obj.B*obj.H^2/4 ...
-                            - 4*((1-pi/4)*obj.r^2)*(obj.H/2-((10-3*pi)/(12-3*pi))*obj.r);
+                            - 4*((1-pi/4)*obj.rc^2)*(obj.H/2-((10-3*pi)/(12-3*pi))*obj.rc);
                     end
                 case {'y','minor','weak'}
-                    if obj.r == 0
+                    if obj.rc == 0
                         z = obj.H*obj.B^2/4;
                     else
                         z = obj.H*obj.B^2/4 ...
-                            - 4*((1-pi/4)*obj.r^2)*(obj.B/2-((10-3*pi)/(12-3*pi))*obj.r);
+                            - 4*((1-pi/4)*obj.rc^2)*(obj.B/2-((10-3*pi)/(12-3*pi))*obj.rc);
                     end
                 otherwise
                     error('Unknown axis: %s',axis);
             end
         end
         function [x,y,r] = boundary_points(obj)
-            if obj.r == 0
+            if obj.rc == 0
                 x = [ obj.B/2 -obj.B/2 -obj.B/2  obj.B/2 ];
                 y = [ obj.H/2  obj.H/2 -obj.H/2 -obj.H/2 ];
                 r = [0 0 0 0];
@@ -122,19 +122,19 @@ classdef Rectangle_Shape < geometric_shape
                 lineWidth = 2;
             end
             hold all
-            if obj.r == 0
+            if obj.rc == 0
                 x = [ obj.B/2 -obj.B/2 -obj.B/2  obj.B/2 obj.B/2 ];
                 y = [ obj.H/2  obj.H/2 -obj.H/2 -obj.H/2 obj.H/2 ];
             else
                 angles = linspace(0,pi/2,25);
-                x = [ ( obj.B/2+obj.r*cos(angles)) ...
-                      (-obj.B/2+obj.r*cos(angles+pi/2)) ...
-                      (-obj.B/2+obj.r*cos(angles+pi)) ...
-                      ( obj.B/2+obj.r*cos(angles+1.5*pi)) obj.B/2+obj.r ];
-                y = [ ( obj.H/2+obj.r*sin(angles)) ...
-                      ( obj.H/2+obj.r*sin(angles+pi/2)) ...
-                      (-obj.H/2+obj.r*sin(angles+pi)) ...
-                      (-obj.H/2+obj.r*sin(angles+1.5*pi)) obj.H/2 ]; 
+                x = [ ( obj.B/2+obj.rc*cos(angles)) ...
+                      (-obj.B/2+obj.rc*cos(angles+pi/2)) ...
+                      (-obj.B/2+obj.rc*cos(angles+pi)) ...
+                      ( obj.B/2+obj.rc*cos(angles+1.5*pi)) obj.B/2+obj.rc ];
+                y = [ ( obj.H/2+obj.rc*sin(angles)) ...
+                      ( obj.H/2+obj.rc*sin(angles+pi/2)) ...
+                      (-obj.H/2+obj.rc*sin(angles+pi)) ...
+                      (-obj.H/2+obj.rc*sin(angles+1.5*pi)) obj.H/2 ]; 
                  
             end
             fill(x,y,obj.plot_fill_color,'LineStyle','none')
@@ -142,7 +142,7 @@ classdef Rectangle_Shape < geometric_shape
             axis equal
         end
         function add_to_fiber_section(obj,fiber_section,matID)
-            if obj.r == 0
+            if obj.rc == 0
                 fiber_section.addPatch('quad',matID,...
                     -obj.B/2,-obj.H/2,...
                     -obj.B/2, obj.H/2,...
